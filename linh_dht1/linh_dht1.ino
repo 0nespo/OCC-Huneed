0d74ba11e09f37d9416ce288859214fd8c6726e9
Ones sanjerico <92374095+0nespo@users.noreply.github.com>
2023-01-30 17:09:11 +0900

    Update linh_dht1.ino

--- linh_dht1/linh_dht1.ino
+++ linh_dht1/linh_dht1.ino
@@ -1,8 +1,5 @@
-// Design by Nguyen Van Linh
-// email: linhnv@kookmin.kr.ac
-
 #include <Adafruit_NeoPixel.h>
-#include "DHT.h"//Khai báo thư viện DHT
+#include "DHT.h"//DHT
 #include <string.h> 
  
 // Which pin on the Arduino is connected to the NeoPixels?
@@ -10,6 +7,9 @@
 #define PIN 9
 #define DHTPIN 4 //DHT22 pin
 #define DHTTYPE DHT22  
+#define Dig_pin 6;
+int Dig_out = LOW;
+int v = 0;
 DHT dht(DHTPIN, DHTTYPE);
  
 // How many NeoPixels are attached to the Arduino?
@@ -42,12 +42,15 @@ void loop() {
  int a[64]= {0,3,4,5,7,56,63,50,51,52,44,42,46,48};
  int h =  ReadH();
  int t = ReadT();
+ int v = ReadV();
   Serial.print("Humidity: ");
   Serial.print(h);
   Serial.print(" %\t");
   Serial.print("Temperature: ");
   Serial.print(t);
   Serial.print(" *C ");
+  Serial.print("Vibration: ");
+  Serial.print(v);
 //Temperature============
       uint8_t bitsCount = sizeof( t ) * 4;
       char str[ bitsCount + 1 ];
@@ -99,7 +102,38 @@ void loop() {
           a[dem]=i+16;
           dem = dem+1;
         }
-        
+      }
+      
+      ///Vibration=====================
+      //int v = Ana_out;
+      uint8_t bitsCount4 = sizeof( v ) * 4;
+      char str3[ bitsCount4 + 1 ];
+      uint8_t m = 0;
+      while ( bitsCount4-- )
+      str3[ m++ ] = bitRead( v, bitsCount4 ) + '0';
+      str3[ m ] = '\0';
+      char str4[8];
+      str4[0]=str3[7]; str4[1]=str3[6]; str4[2]=str3[5]; str4[3]=str3[4]; str4[4]=str3[3]; str4[5]=str3[2]; str4[6]=str3[1]; str4[7]=str3[0]; 
+       
+      int arrSize4 = sizeof(str4)/sizeof(str4[0]);
+     // Serial.println(dem);
+      for(int i=0; i<arrSize4 ; i++){
+        Serial.println("Vibration: ");
+        //Serial.println(str3);
+
+        Serial.println(str4);
+        Serial.println(v); // (v-100)
+
+        if (str4[i]=='1')
+        {
+          Serial.println(i);
+         // a[6]=8;
+          a[dem]=i+32;
+          //Serial.println(dem);
+          dem = dem+3;
+        }
+
+      //================================
       }
 
       //================================
@@ -131,6 +165,10 @@ int ReadH(){
 int ReadT(){
   int t = dht.readTemperature();
     return t;
-      //delay(1000);
-   
+      //delay(1000);   
+}
+int ReadV(){
+   v = analogRead(A1);
+   return v;
+   //delay(1000);
 }
